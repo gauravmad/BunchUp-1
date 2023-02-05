393cca44acbf4b19d8f696445b0d6028fdb87c72
blackhackerz <60981503+blackhackerz@users.noreply.github.com>
2023-02-05 19:33:57 +0530

    made site responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,30 @@
 
 var i = 0;
-var txt = `Be prepare to connect with individuals who share your interests and create strong communities with BunchUp.
-Mark your calendars, we'll be launching soon.`;
-var speed = 100;
+var txt = `Be prepared to connect with random people who share same interests and have fun together. So what are you waiting for mark your calendars, We’ll be launching soon…`;
+var speed = 150;
 
 function type() {
     if (i < txt.length) {
-      document.getElementById("comming_soon_sub_heading").innerHTML += txt.charAt(i);
+      document.getElementById("coming_soon_sub_heading").innerHTML += txt.charAt(i);
       i++;
       setTimeout(type, speed);
     }else{
-      document.getElementById("comming_soon_sub_heading").classList.add("blink_cursor")
+      document.getElementById("coming_soon_sub_heading").classList.add("blink_cursor")
     }
   }
+
+  function nav_bar(x) {
+    x.classList.toggle("change");
+    const width = document.getElementById('sidebar').style.width;
+    if(width !='0px'){
+      document.getElementById('sidebar').style.width = '0px';
+      document.getElementById('content_container').style.display = 'block'
+      document.getElementById('sidebar').style.display = 'none';
+
+    }else{
+      
+      document.getElementById('sidebar').style.display = 'block';
+      document.getElementById('sidebar').style.width = '200px';
+      document.getElementById('content_container').style.display = 'none';
+    }
+  } 
